6327eacc0945a4c11e3649f9e5ca4e13fd184b69
tiennx <tiennx@vmodev.com>
2022-12-20 10:29:52 +0700

    update background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-    <div class="Background">Background Layer</div>
+    <div class="Background">
+        Background Layer
+    </div>
 </template>
 
 <style scoped>
@@ -11,6 +13,9 @@
     width: 100%;
     height: 100%;
     /*background: var(--tm-page-bg);*/
-    background: red;
+    /*background: red;*/
+    /*background: url("/img/vivamagenta.jpg");*/
+    background: linear-gradient(to right, #673ab7, #bfc3d9);
+    text-align: right;
 }
 </style>
